ab805d05d6d88dd80350d1f5cfa2972ae60e3ef6
EC2 Default User <ec2-user@ip-172-31-73-213.ec2.internal>
2022-01-26 20:04:41 +0000

    lecture 2 notes

---
+++ lab2.py
@@ -0,0 +1,24 @@
+'''
+lec 2
+this is a regional comment 
+'''
+
+
+#print(type(123)) #this is a single line comment 
+
+#print(type(123.))
+
+#print(type("123"))
+
+#print("Tom's cat")
+
+#print("Hello " + "world")
+
+#y_int = 123
+#int(my_int)
+
+#y_int = 321
+#rint(my_int)
+
+my_str = 'hello world'
+print(my_str)
