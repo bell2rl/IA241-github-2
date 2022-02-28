b93bfb87609ba49810016a5b49e8de4e1caafc2a
EC2 Default User <ec2-user@ip-172-31-73-213.ec2.internal>
2022-02-28 19:53:50 +0000

    Lab 5 is now uploaded

---
+++ Lab5.py
@@ -0,0 +1,61 @@
+"""
+Lab 5 for Python Class
+"""
+
+#3.1
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('You earned 5 points!')
+    
+
+#3.2
+
+if alien_color == 'green':
+    print('You earned 5 points!')
+    
+else: alien_color == 'red','yellow'
+print(
+            'You earned 10 points')
+            
+            
+#3.3
+
+favorite_fruits = ['apples', 'pears', 'peach']
+
+if 'apples' in favorite_fruits :
+    print('You really like apples!')
+
+if 'pears' in favorite_fruits:
+    print(
+        'you really like pears!')
+        
+if 'peach' in favorite_fruits:
+    print(
+        'you really like peach!')
+        
+        
+
+#3.4
+
+persons_age = 9
+
+if persons_age <10:
+    print('kid')
+    
+elif persons_age >=10 and persons_age <20:
+        print( 'teenager')
+
+else:
+    print('adult')
+    
+    
+    
+    if persons_age>=65:
+        print('elder')
+        
+    
+    
+    
+
---
+++ tets1.py
@@ -0,0 +1,5 @@
+demo_list = ['12', 12, 13]
+
+demo_list.remove('12')
+
+print(demo_list)
